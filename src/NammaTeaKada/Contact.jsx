0c397ec66db5533fe5132424aa28b74bbc391b83
gokulkailash2003 <kailashgokul2003@gmail.com>
2025-12-02 21:53:45 +0530

    Update Contact.jsx to use Render backend URL

--- src/NammaTeaKada/Contact.jsx
+++ src/NammaTeaKada/Contact.jsx
@@ -66,7 +66,7 @@ function Contact(){
           return;
         }
 
-        fetch('http://localhost:5001/send-mail', {
+        fetch('https://namma-teakada-1.onrender.com/send-mail', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
